a6e68f66cbcbf72b2a1d3d097bc3105507020d79
James Keicher <jamesdkeicher@gmail.com>
2022-02-25 11:33:22 -0800

    chore: remove unused code from RatingDisplay

--- src/ratingDisplay.js
+++ src/ratingDisplay.js
@@ -7,7 +7,7 @@ export default function RatingDisplay() {
   // Catch Rating value
   const handleRating = (rate) => {
     setRating(Number(rate));
-    // other logic
+    console.log(rate / 20);
   };
 
   return (
@@ -16,42 +16,3 @@ export default function RatingDisplay() {
     </div>
   );
 }
-
-// const fullStar = '★';
-// const emptyStar = '☆';
-// import React from 'react';
-
-// export default function RatingDisplay({ rating }) {
-//   const rates = stars(rating);
-//   console.log(fullStar);
-//   return <p>{rates}</p>;
-// }
-
-// export const stars = (rating) => {
-//   let totalRating;
-//   switch (rating) {
-//     case 1:
-//       totalRating = `${(<span>{fullStar}</span>)}${emptyStar}${emptyStar}${emptyStar}${emptyStar}`;
-//       break;
-//     case 2:
-//       totalRating = `${fullStar}${fullStar}${emptyStar}${emptyStar}${emptyStar}`;
-//       break;
-//     case 3:
-//       totalRating = `${fullStar}${fullStar}${fullStar}${emptyStar}${emptyStar}`;
-//       break;
-//     case 4:
-//       totalRating = `${fullStar}${fullStar}${fullStar}${fullStar}${emptyStar}`;
-//       break;
-//     case 5:
-//       totalRating = `${fullStar}${fullStar}${fullStar}${fullStar}${fullStar}`;
-//       break;
-
-//     default:
-//       totalRating = `${(<span>{fullStar}</span>)}`;
-//       break;
-//   }
-//   //   console.log(totalRating);
-//   return totalRating;
-// };
-
-// // export const battleUserRating = () => {};
