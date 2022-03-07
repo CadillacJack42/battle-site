f1a0554f340d5425df7ce96b0bea08e0c2cdde81
James Keicher <jamesdkeicher@gmail.com>
2022-03-07 09:58:22 -0800

    feat: saving user ratings in rating object

--- src/ratingDisplay.js
+++ src/ratingDisplay.js
@@ -3,6 +3,8 @@ import { Rating } from 'react-simple-star-rating';
 import { updateRatings, fetchAllBattles, fetchRating } from './services/fetch-utils';
 
 export default function RatingDisplay({ profile, battleId, contender, setBattles }) {
+  const username = profile.username;
+
   const [userRating, setUserRating] = useState(0); // initial rating value
   const [currentRatings, setCurrentRatings] = useState({});
   const [updatedRatings, setUpdatedRatings] = useState({});
@@ -17,21 +19,16 @@ export default function RatingDisplay({ profile, battleId, contender, setBattles
   }, [battleId, contender, profile.username]);
 
   useEffect(() => {
-    setUpdatedRatings({ [`${contender}_ratings`]: userRating });
+    const newRating = { ...currentRatings[`${contender}_rating`] };
+    newRating[username] = userRating;
+    console.log(newRating[username]);
+    setUpdatedRatings(newRating);
   }, [currentRatings, contender, userRating]);
 
   useEffect(() => {
-    updateRatings(battleId, { [`${profile.username}`]: userRating }, contender);
-  }, [battleId, contender, userRating]);
-
-  // useEffect(() => {
-  //   const setNewRatings = async () => {
-  //     console.log(updatedRatings);
-  //     const response = await updateRatings(battleId, updatedRatings, contender);
-  //     console.log(response);
-  //   };
-  //   setNewRatings();
-  // }, [updatedRatings]);
+    console.log(updatedRatings);
+    userRating !== 0 && updateRatings(battleId, updatedRatings, contender);
+  }, [battleId, contender, userRating, updatedRatings]);
 
   const handleUserRating = async (e) => {
     console.log(e);
