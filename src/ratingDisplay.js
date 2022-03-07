09e05b7d63159434d463c50aab72c4f394a716e6
James Keicher <jamesdkeicher@gmail.com>
2022-03-06 21:39:36 -0800

    Refactor: Rewriting RatingsDisplay Initial Commit

--- src/ratingDisplay.js
+++ src/ratingDisplay.js
@@ -3,44 +3,14 @@ import { Rating } from 'react-simple-star-rating';
 import { updateRatings, fetchAllBattles, fetchRating } from './services/fetch-utils';
 
 export default function RatingDisplay({ profile, battleId, contender, setBattles }) {
-  const [userRating, setUserRating] = useState(0); // initial rating value
-  // const [overallRating, setOverallRating] = useState(0);
-
-  let currentRatings;
-  useEffect(() => {
-    const getCurrRating = async () => {
-      const rating = await fetchRating(battleId);
-      console.log(rating[0]);
-      (await rating) && (currentRatings = JSON.parse(rating[0][`${contender}_rating`]));
-      setUserRating(currentRatings);
-      console.log(currentRatings);
-    };
-    getCurrRating();
-  }, []);
-
-  useEffect(() => {
-    const setRating = async () => {
-      const updatedRating = {
-        username: profile.username,
-        [`${contender}_rating`]: userRating,
-      };
-
-      let newRatings;
-      currentRatings
-        ? (newRatings = [currentRatings, updatedRating])
-        : (newRatings = [updatedRating]);
-
-      await updateRatings(battleId, newRatings, contender);
-      const updatedBattles = await fetchAllBattles();
-      setBattles(updatedBattles);
-    };
-    setRating();
-  }, [userRating]);
+  const [userRating, setUserRating] = useState(0); // STARS initial rating value
+  const [currentRatings, setCurrentRatings] = useState({});
+  const [updatedRatings, setUpdatedRatings] = useState({});
 
   const handleUserRating = async (e) => {
+    console.log(e);
     setUserRating(e);
   };
-
   return (
     <div className="Rating">
       <Rating onClick={handleUserRating} ratingValue={userRating} /* Available Props */ />
